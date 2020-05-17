31f42ee5f1372301edac10e4b3700b74e08efeef
Clarisse <lou.claris@gmail.com>
2020-05-17 16:17:40 +0200

    ajout de la map

--- cine-boug/src/app/map/map.component.ts
+++ cine-boug/src/app/map/map.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { defaults as defaultControls } from 'ol/control';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import * as Proj from 'ol/proj';
 
 @Component({
   selector: 'map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
 
-  constructor() { }
+  map: Map;
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: Proj.fromLonLat([2.3488, 48.8534]),
+        zoom: 2
+      }),
+      controls: defaultControls().extend([])
+    });
+
+    this.map.setTarget('map');
   }
-
 }
